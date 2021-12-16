3ffecec056c3ca6762dc1fc346d56cbe3870939a
GoldenPaladin <yacine.ezdini@gmail.com>
2021-12-16 15:20:28 +0100

    user schema + all type of create/save

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const User = require("./user");
 
 mongoose.connect(
   "mongodb://localhost/testdb",
@@ -7,3 +8,17 @@ mongoose.connect(
   },
   (e) => console.log(e)
 );
+
+// save is an asynchronous function so we could use .then
+// const user = new User({ name: "Yacine", age: 22 });
+// user.save().then(() => console.log("user saved"));
+
+// console.log(user);
+
+async function run() {
+  //const user = await User.create({ name: "Hannibal", age: 18})
+  const user = new User({ name: "Toutou", age: 25 });
+  await user.save();
+  console.log(user);
+}
+run();
