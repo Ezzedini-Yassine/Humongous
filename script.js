75fc40a7f53419f6c2377dab99b06b18244f0015
GoldenPaladin <yacine.ezdini@gmail.com>
2021-12-17 01:57:14 +0100

    adding a new ObjectId of type user to a user

--- script.js
+++ script.js
@@ -16,15 +16,19 @@ mongoose.connect(
 
 async function run() {
   try {
-    // find user where age === 18 and name: Diego, limit 2, select the fields
-    // you want to be returned
+    //add bestfriend to a user, "look at schema, bestfriend is ref of user"
     const user = await User.where("age")
       .equals(18)
       .lt(35)
       .where("name")
       .equals("Diego")
       .limit(2)
-      .select("hobbies");
+      .select("age");
+    //here, we are retriving a table of users from previous query
+    //now we will add a bestfriend to user 1 from that table of returned users
+    //PS: if you are having a fresh DB this wont work cause user doesnt exist
+    user[0].bestFriend = "61bbb68be783ff36bcb91632";
+    await user[0].save();
     console.log(user);
   } catch (e) {
     console.log(e.message);
