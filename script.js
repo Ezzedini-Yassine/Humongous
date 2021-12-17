d9add0f5cc901c429ad6f23341e65a0d74dff1f0
GoldenPaladin <yacine.ezdini@gmail.com>
2021-12-17 01:20:10 +0100

    multiple where + limit + select

--- script.js
+++ script.js
@@ -9,11 +9,22 @@ mongoose.connect(
   (e) => console.log(e)
 );
 
+//the syntax for the find method is confusing in mongoDB which means
+//it's confusing in mongoose, which why mongoose implemented something
+//named queries -> User.where(), it allows you to create your own query
+//based on a bunch of helper methods
+
 async function run() {
   try {
-    // there is also deleteMany
-    // the console log of user will return this: { deletedCount: 6 }
-    const user = await User.deleteMany({ name: "Hannibal" });
+    // find user where age === 18 and name: Diego, limit 2, select the fields
+    // you want to be returned
+    const user = await User.where("age")
+      .equals(18)
+      .lt(35)
+      .where("name")
+      .equals("Diego")
+      .limit(2)
+      .select("hobbies");
     console.log(user);
   } catch (e) {
     console.log(e.message);
